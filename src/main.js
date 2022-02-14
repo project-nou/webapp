8e6751bebf2178b44b91c603da696b493f6e1a33
Florian Berrotµ <florian.berrot@ynov.com>
2022-02-14 15:06:15 +0100

    feat: add vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 // import VueAxios from 'vue-axios';
-// import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false;
 
